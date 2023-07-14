076b75ff99fe57e1ba2b2a4d36835a1bdc8e5152
andycc77 <andycc77e@gmail.com>
2023-07-14 14:34:13 +0800

    新增關於頁面

--- main.go
+++ main.go
@@ -6,7 +6,15 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 這裡是 goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 這裡是 goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "請聯絡 "+
+			"<a href=\"mailto:andycc77e@gmail.com\">andycc77e@gmail.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>請求頁面未找到 :(</h1>"+
+			"<p>如有疑惑，請聯絡我們。</p>")
+	}
 }
 
 func main() {
