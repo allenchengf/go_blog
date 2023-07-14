d478711418173305aa128cc8c4848e20ded285cb
andycc77 <andycc77e@gmail.com>
2023-07-14 15:40:13 +0800

    集成Gorilla Mux路由

--- main.go
+++ main.go
@@ -3,27 +3,59 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 這裡是 goblog</h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>請求頁面未找到 :(</h1>"+
-			"<p>如有疑惑，請聯絡我們。</p>")
-	}
+	fmt.Fprint(w, "<h1>Hello, 歡迎來到 goblog！</h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "請聯絡我 "+
+	fmt.Fprint(w, "請聯絡 "+
 		"<a href=\"mailto:andycc77e@gmail.com\">andycc77e@gmail.com</a>")
 }
 
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>頁面未找到 :(</h1><p>如有疑惑，請聯繫我們。</p>")
+}
+
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprint(w, "文章 ID："+id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "文章列表")
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "創建新的文章")
+}
+
 func main() {
-	http.HandleFunc("/", defaultHandler)
-	http.HandleFunc("/about", aboutHandler)
-	http.ListenAndServe(":3000", nil)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+
+	// 自定義404頁面
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	// 通過命名路由獲取URL實例
+	homeURL, _ := router.Get("home").URL()
+	fmt.Println("homeURL: ", homeURL)
+	articleURL, _ := router.Get("articles.show").URL("id", "23")
+	fmt.Println("articleURL: ", articleURL)
+
+	http.ListenAndServe(":3000", router)
 }
